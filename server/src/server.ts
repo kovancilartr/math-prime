f3b1e3d1f7702cff0b1770ff0f8e117e3fb1a666
Yalçın <kovancilartr@gmail.com>
2025-01-31 03:19:30 +0300

    v1.0.4

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://math-prime-client.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
