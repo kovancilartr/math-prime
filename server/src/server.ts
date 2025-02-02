5a97f3377611e27d68518966ccc9b91faa02a4a0
Yalçın <kovancilartr@gmail.com>
2025-02-03 01:11:17 +0300

    v.1.1.3 token düzenlemesi yapıldı

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,8 +20,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  // origin: ["https://math-prime.vercel.app"],
-  origin: ["http://localhost:3000"],
+  origin: ["https://math-prime.vercel.app"],
+  // origin: ["http://localhost:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
